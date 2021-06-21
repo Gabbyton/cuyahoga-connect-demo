015c76dafe5f7d49762e787e74b24000331baa62
startec-official <startec.innovations@gmail.com>
2021-06-21 13:10:39 -0400

    added route guard to add event

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { AddEventComponent } from './pages/add-event/add-event.component';
 import { EventComponent } from './pages/event/event.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { AngularFireAuthGuard, hasCustomClaim, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['profile']);
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'add-event', component: AddEventComponent },
+  { path: 'add-event', component: AddEventComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
   { path: 'profile', component: ProfileComponent },
   { path: 'event/:id', component: EventComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
