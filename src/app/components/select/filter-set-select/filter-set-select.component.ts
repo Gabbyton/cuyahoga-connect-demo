8dddee122b9b25448a1db1f33b7b9ee0ca5a214e
startec-official <startec.innovations@gmail.com>
2021-06-24 10:28:50 -0400

    added functional filter select with remove

--- src/app/components/select/filter-set-select/filter-set-select.component.ts
+++ src/app/components/select/filter-set-select/filter-set-select.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Filter } from 'src/app/utils/data/models/filter.model';
 import { FilterService } from 'src/app/utils/services/model-services/filter.service';
 
@@ -15,21 +15,45 @@ export class FilterSetSelectComponent implements OnInit {
     'Oh, nevermind',
   ];
 
-  currentlySelectedFilters: Filter[] = [];
+  currentlySelectedFilters: (Filter | null)[] = [null];
   currentSelectorContent: string | null = null;
+  currentlyHighlightedFilter: number = -1;
 
   constructor(private filterService: FilterService) { }
 
   ngOnInit(): void {
-    this.currentlySelectedFilters = [...this.filters];
   }
 
-  get filters() {
+  get filters(): readonly Filter[] {
     return this.filterService.filters;
+  }
+
+  get wheelchairFilter(): Filter {
+    return this.filters.filter(filter => filter.shortName == 'wheelchair')[0];
+  }
+
+  addFilter(newFilter: Filter): void {
+    if (this.currentlySelectedFilters[0] == null) {
+      this.currentlySelectedFilters.splice(0, 1);
+    }
+    this.currentlySelectedFilters.push(newFilter);
+  }
+
+  get filterMenuIndexForWheelchair() {
+    return FilterMenuOption.WheelchairAccessibility;
   }
 
   selectOption(index: number) {
     switch (index) {
+      case FilterMenuOption.WheelchairAccessibility:
+        this.addFilter(this.wheelchairFilter);
+        break;
+      case FilterMenuOption.RemoveFilter:
+        this.currentlySelectedFilters.splice(this.currentlyHighlightedFilter, 1);
+        if (this.currentlySelectedFilters.length <= 0) {
+          this.currentlySelectedFilters.push(null);
+        }
+        break;
     }
   }
 }
