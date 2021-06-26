e8bd9a4c755d379e2e1b004b80585f7631230cb9
startec-official <startec.innovations@gmail.com>
2021-06-26 11:40:08 -0400

    replaced environment to default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment.dev';
+import { environment } from '../environments/environment';
 import { HomeComponent } from './pages/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-c7jhiMX5cAV9jC_4AmyN-15Y5wC--pw",
+    authDomain: "cuyahoga-connect-demo.firebaseapp.com",
+    projectId: "cuyahoga-connect-demo",
+    storageBucket: "cuyahoga-connect-demo.appspot.com",
+    messagingSenderId: "497923986323",
+    appId: "1:497923986323:web:fcc22a7ec103539b754e66",
+    measurementId: "G-3WMZMLT1VC",
+}
 };
